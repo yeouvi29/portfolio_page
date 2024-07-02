6dfb18fdc0c283232499004d8c1c0e3ec8cb0461
Emily Kang <yeouvi29@gmail.com>
2024-07-02 10:11:13 -0700

    chore: update page description

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -9,8 +9,9 @@ const Page = () => {
       <p>
         This portfolio page serves as a comprehensive and dynamic interface
         designed to showcase the skills while offering seamless navigation to
-        various essential sections. It is built with a modern tech stack,
-        emphasizing responsiveness, user experience, and functionality.
+        various essential sections. Built with Next.js, TailwindCSS, MongoDB,
+        and GraphQL, it highlights responsiveness, user experience, and
+        functionality.
       </p>
     </div>
   );
