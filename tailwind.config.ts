a97494e336705698f409cf22b6708e9a6274618c
Emily Kang <yeouvi29@gmail.com>
2024-07-10 16:51:10 -0700

    chore: set default style configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["'Open Sans'", "sans-serif"],
+      display: ["'Nunito Sans'", "sans-serif"],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1920px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
