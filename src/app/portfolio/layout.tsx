d59ee453f6bccf704f7a89172220f672423b8225
Emily Kang <yeouvi29@gmail.com>
2024-07-16 22:47:14 -0700

    style:  update style for mobile view

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -3,7 +3,7 @@ import SideNavigation from "../components/sections/SideNavigation/SideNavigation
 import { ApolloWrapper } from "../ApolloWrapper";
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-row">
+    <div className="min-h-screen flex flex-col md:flex-row">
       <SideNavigation />
       <ApolloWrapper>
         <div className="w-full md:w-[calc(100%-250px)] p-5">{children}</div>
