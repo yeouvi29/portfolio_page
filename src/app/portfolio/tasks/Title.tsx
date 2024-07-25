372daa9a68fbc946cda5e22909d6a7ed15d7313d
Emily Kang <yeouvi29@gmail.com>
2024-07-24 21:07:23 -0700

    feat: add title component

---
+++ src/app/portfolio/tasks/Title.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { FormEvent, useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+
+import ClickAwayListener from "@/app/components/common/ClickAwayLIstener/ClickAwayListener";
+
+const Title = () => {
+  const titleRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [titleInput, setTitleInput] = useState("Task Management");
+  const [isEditable, setIsEditable] = useState(false);
+  const [titleWidth, setTitleWidth] = useState(0);
+  const lastSavedTitle = useRef(titleInput);
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setIsEditable(false);
+  };
+
+  const handleClickAway = () => {
+    setIsEditable(false);
+  };
+
+  useEffect(() => {
+    if (!isEditable) {
+      return;
+    }
+
+    lastSavedTitle.current = titleInput;
+    inputRef.current?.focus();
+    inputRef.current?.select();
+  }, [isEditable]);
+
+  useEffect(() => {
+    if (!isEditable && !titleInput) {
+      setTitleInput(lastSavedTitle.current);
+    }
+  }, [titleInput, isEditable]);
+
+  useEffect(() => {
+    const inputEl = inputRef.current;
+    if (!inputEl) return;
+
+    const tempSpan = document.createElement("span");
+
+    tempSpan.style.visibility = "hidden";
+    tempSpan.style.position = "absolute";
+    tempSpan.style.whiteSpace = "pre";
+    tempSpan.style.fontSize = window.getComputedStyle(inputEl).fontSize;
+    tempSpan.style.fontFamily = window.getComputedStyle(inputEl).fontFamily;
+    tempSpan.style.fontWeight = window.getComputedStyle(inputEl).fontWeight;
+    tempSpan.style.fontStyle = window.getComputedStyle(inputEl).fontStyle;
+    tempSpan.style.letterSpacing =
+      window.getComputedStyle(inputEl).letterSpacing;
+
+    document.body.appendChild(tempSpan);
+
+    tempSpan.textContent = inputEl.value || inputEl.placeholder;
+    const width = tempSpan.offsetWidth + 9;
+
+    setTitleWidth(width);
+    document.body.removeChild(tempSpan);
+  }, [titleInput]);
+
+  return (
+    <div className="w-full flex">
+      <ClickAwayListener onClickAway={() => handleClickAway()}>
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            type="text"
+            value={titleInput}
+            onChange={(e) => {
+              setTitleInput(e.target.value);
+            }}
+            className={clsx(
+              "text-lg font-bold bg-transparent font-display p-1 rounded-lg",
+              !isEditable && "hidden"
+            )}
+            style={{ width: titleWidth }}
+          />
+          <div
+            ref={titleRef}
+            className={clsx(
+              "p-1 rounded-lg cursor-pointer hover:bg-slate-300/60",
+              isEditable && "hidden"
+            )}
+            onClick={() => setIsEditable(true)}
+          >
+            <h1 className="text-lg whitespace-pre">{titleInput}</h1>
+          </div>
+        </form>
+      </ClickAwayListener>
+    </div>
+  );
+};
+
+export default Title;
---
+++ src/app/portfolio/tasks/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/app/components/common/Container/Container";
+import Title from "./Title";
+
+const Page = () => {
+  return (
+    <Container>
+      <Title />
+    </Container>
+  );
+};
+
+export default Page;
