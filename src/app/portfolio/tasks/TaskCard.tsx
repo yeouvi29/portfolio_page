b7c0e7fa465a6a520454014741ddf2dc5c9d0a43
Emily Kang <yeouvi29@gmail.com>
2024-07-25 20:52:47 -0700

    fix: apply style to a list element when the drag event is triggered

--- src/app/portfolio/tasks/TaskCard.tsx
+++ src/app/portfolio/tasks/TaskCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import clsx from "clsx";
-import { DragEvent, Fragment, useState } from "react";
+import { DragEvent, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import styles from "./styles.module.css";
+
 const TaskCard = ({
   task,
   title,
@@ -17,6 +17,7 @@ const TaskCard = ({
   dragItem: any;
   onDragStart: (item: any) => void;
 }) => {
+  const listRef = useRef<HTMLLIElement>(null);
   const [isDraggedOver, setIsDraggedOver] = useState(false);
   const handleDragStart = (event: any) => {
     const height = event.target.clientHeight;
@@ -41,7 +42,7 @@ const TaskCard = ({
     event.dataTransfer.setDragImage(dragImage, 0, 0);
 
     setTimeout(() => {
-      event.target.style.display = "none";
+      (listRef?.current as HTMLLIElement).style.display = "none";
     }, 0);
   };
 
@@ -53,11 +54,12 @@ const TaskCard = ({
 
   const handleDragEnd = (event: any) => {
     onDragStart(null);
-    event.target.style.display = "block";
+    (listRef?.current as HTMLLIElement).style.display = "block";
   };
 
   return (
     <li
+      ref={listRef}
       className={clsx(
         "relative w-[272px] bg-white transition-all duration-300 ease-in-out"
       )}
