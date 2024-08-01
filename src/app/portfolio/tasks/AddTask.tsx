98b65f00f7ff5586ff501bc0a44c4a687880b551
Emily Kang <yeouvi29@gmail.com>
2024-08-01 12:06:16 -0700

    feat: add adding card logic

--- src/app/portfolio/tasks/AddTask.tsx
+++ src/app/portfolio/tasks/AddTask.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { DragEvent } from "react";
+import { DragEvent, Fragment, useEffect, useRef, useState } from "react";
+import clsx from "clsx";
+import { v4 as uuidv4 } from "uuid";
+import { IoClose } from "react-icons/io5";
 
 import { useTaskItems } from "@/store";
-
+import { TaskItem, TaskItems } from "@/types";
+interface NewTask {
+  text: string;
+}
 const AddTask = ({
   columnId,
   tasksLength,
@@ -10,7 +16,13 @@ const AddTask = ({
   columnId: string;
   tasksLength: number;
 }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [setDragEnterItem] = useTaskItems(({ setDragEnter }) => [setDragEnter]);
+  const [newTask, setNewTask] = useState<NewTask | null>(null);
+  const [taskItems, setTaskItems] = useTaskItems(({ items, setTaskItems }) => [
+    items,
+    setTaskItems,
+  ]);
 
   const handleDragEnter = (e: DragEvent) => {
     e.stopPropagation();
@@ -22,14 +34,108 @@ const AddTask = ({
     });
   };
 
+  const handleClick = () => {
+    setNewTask({ text: "" });
+  };
+  const handleClose = () => {
+    setNewTask(null);
+  };
+  const updateTask = (text: string) => {
+    const task = {
+      id: uuidv4(),
+      text: text || "",
+    };
+    const updatedTasks = taskItems.map((item: TaskItems) => {
+      if (item.id === columnId) {
+        return {
+          ...item,
+          items: [...item.items, task],
+        };
+      }
+      return item;
+    });
+
+    setTaskItems(updatedTasks);
+  };
+  const handleClickOnAddCard = () => {
+    if (newTask?.text.trim() === "") {
+      setNewTask(null);
+      return;
+    }
+    if (newTask) {
+      updateTask(newTask.text.trim());
+    }
+    setNewTask({ text: "" });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask({ text: e.target.value });
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!newTask || newTask.text.trim() === "") {
+      setNewTask(null);
+      return;
+    }
+
+    updateTask(newTask.text.trim());
+    setNewTask(null);
+  };
+
+  useEffect(() => {
+    if (!newTask) return;
+    if (newTask.text.trim() === "") {
+      inputRef.current?.focus();
+    }
+  }, [newTask]);
   return (
-    <div
-      className="w-full mt-2 p-2 group relative cursor-pointer rounded-lg hover:bg-gray-400/30 pointer-events-none"
-      onDragOver={(e) => e.preventDefault()}
-      onDragEnter={handleDragEnter}
-    >
-      + Add a card
-    </div>
+    <Fragment>
+      {newTask && (
+        <div
+          className={clsx(
+            "w-[272px] bg-white rounded-lg  my-1 overflow-hidden"
+          )}
+          style={{ minHeight: 40 }}
+          data-draggedover={true}
+        >
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={inputRef}
+              value={newTask?.text || ""}
+              className="w-full h-full p-2 outline-none"
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      )}
+      {!newTask && (
+        <button
+          className="w-full mt-2 p-2 rounded-lg text-left hover:bg-gray-400/30"
+          onClick={handleClick}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={handleDragEnter}
+        >
+          + Add a card
+        </button>
+      )}
+      {newTask && (
+        <div className="w-full mt-2 h-[32px] flex gap-2 items-center">
+          <button
+            className="h-full bg-blue-600 px-2 rounded-md text-white"
+            onClick={handleClickOnAddCard}
+          >
+            Add card
+          </button>
+          <button
+            className="hover:bg-gray-400/30 rounded-md h-full aspect-square text-[20px] text-gray-800 flex items-center justify-center"
+            onClick={handleClose}
+          >
+            <IoClose />
+          </button>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
