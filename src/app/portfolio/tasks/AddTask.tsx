c6585d86fe94f1b69b54c40808368703db771c6c
Emily Kang <yeouvi29@gmail.com>
2024-08-01 17:50:53 -0700

    feat: add ClickAwayListener to handle click events outside of the button

--- src/app/portfolio/tasks/AddTask.tsx
+++ src/app/portfolio/tasks/AddTask.tsx
@@ -1,11 +1,19 @@
 "use client";
-import { DragEvent, Fragment, useEffect, useRef, useState } from "react";
+import {
+  DragEvent,
+  Fragment,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import clsx from "clsx";
 import { v4 as uuidv4 } from "uuid";
 import { IoClose } from "react-icons/io5";
 
 import { useTaskItems } from "@/store";
-import { TaskItem, TaskItems } from "@/types";
+import { TaskItems } from "@/types";
+import ClickAwayListener from "@/app/components/common/ClickAwayLIstener/ClickAwayListener";
 interface NewTask {
   text: string;
 }
@@ -23,7 +31,7 @@ const AddTask = ({
     items,
     setTaskItems,
   ]);
-
+  const newTaskRef = useRef(newTask);
   const handleDragEnter = (e: DragEvent) => {
     e.stopPropagation();
 
@@ -37,9 +45,11 @@ const AddTask = ({
   const handleClick = () => {
     setNewTask({ text: "" });
   };
+
   const handleClose = () => {
     setNewTask(null);
   };
+
   const updateTask = (text: string) => {
     const task = {
       id: uuidv4(),
@@ -68,9 +78,19 @@ const AddTask = ({
     setNewTask({ text: "" });
   };
 
+  const handleClickAway = useCallback(() => {
+    const task = newTaskRef.current?.text.trim() || "";
+
+    if (task) {
+      updateTask(task);
+    }
+    setNewTask(null);
+  }, [updateTask]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewTask({ text: e.target.value });
   };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -85,30 +105,14 @@ const AddTask = ({
 
   useEffect(() => {
     if (!newTask) return;
+    newTaskRef.current = newTask;
     if (newTask.text.trim() === "") {
       inputRef.current?.focus();
     }
   }, [newTask]);
+
   return (
     <Fragment>
-      {newTask && (
-        <div
-          className={clsx(
-            "w-[272px] bg-white rounded-lg  my-1 overflow-hidden"
-          )}
-          style={{ minHeight: 40 }}
-          data-draggedover={true}
-        >
-          <form onSubmit={handleSubmit}>
-            <input
-              ref={inputRef}
-              value={newTask?.text || ""}
-              className="w-full h-full p-2 outline-none"
-              onChange={handleChange}
-            />
-          </form>
-        </div>
-      )}
       {!newTask && (
         <button
           className="w-full mt-2 p-2 rounded-lg text-left hover:bg-gray-400/30"
@@ -120,20 +124,39 @@ const AddTask = ({
         </button>
       )}
       {newTask && (
-        <div className="w-full mt-2 h-[32px] flex gap-2 items-center">
-          <button
-            className="h-full bg-blue-600 px-2 rounded-md text-white"
-            onClick={handleClickOnAddCard}
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <div
+            className={clsx(
+              "w-[272px] bg-white rounded-lg  my-1 overflow-hidden"
+            )}
+            style={{ minHeight: 40 }}
+            data-draggedover={true}
           >
-            Add card
-          </button>
-          <button
-            className="hover:bg-gray-400/30 rounded-md h-full aspect-square text-[20px] text-gray-800 flex items-center justify-center"
-            onClick={handleClose}
-          >
-            <IoClose />
-          </button>
-        </div>
+            <form onSubmit={handleSubmit}>
+              <input
+                ref={inputRef}
+                value={newTask?.text || ""}
+                className="w-full h-full p-2 outline-none"
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+
+          <div className="w-full mt-2 h-[32px] flex gap-2 items-center">
+            <button
+              className="h-full bg-blue-600 px-2 rounded-md text-white"
+              onClick={handleClickOnAddCard}
+            >
+              Add card
+            </button>
+            <button
+              className="hover:bg-gray-400/30 rounded-md h-full aspect-square text-[20px] text-gray-800 flex items-center justify-center"
+              onClick={handleClose}
+            >
+              <IoClose />
+            </button>
+          </div>
+        </ClickAwayListener>
       )}
     </Fragment>
   );
