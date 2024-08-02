35056e4cb2c12b1bf27880da9a55a852b5d36df4
Emily Kang <yeouvi29@gmail.com>
2024-08-02 14:05:18 -0700

    style: add title & logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
+import clsx from "clsx";
+
+import { montserrat } from "@/styles/fonts";
+
+import styles from "./styles.module.css";
+
 const Page = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>
-        This portfolio page serves as a comprehensive and dynamic interface
-        designed to showcase the skills while offering seamless navigation to
-        various essential sections. Built with Next.js, TailwindCSS, MongoDB,
-        and GraphQL, it highlights responsiveness, user experience, and
-        functionality.
-      </p>
+    <div className="w-full max-w-screen-xl mt-5 flex gap-5 justify-between">
+      <div className="text-center lg:text-left">
+        <h1 className={clsx(montserrat.className, "mb-5 text-blue-700")}>
+          Emily's Portfolio
+        </h1>
+        <h2 className="text-2xl text-blue-500 mb-3">
+          Take a look at my latest work
+          <br /> where each page showcases different features designed
+          <br /> to address specific user needs.
+        </h2>
+        <p className="text-gray-500">
+          This dynamic interface highlights my skills while offering seamless
+          navigation to essential sections.
+        </p>
+        <p className="text-gray-500">
+          Built with Next.js, TailwindCSS, MongoDB, and GraphQL, it highlights
+          responsiveness, user experience, and functionality.
+        </p>
+      </div>
+      <div className="hidden lg:block">
+        <div
+          className={clsx(
+            "w-[30px] h-[30px] md:h-[250px] md:w-[250px]",
+            styles.heroIcon
+          )}
+        ></div>
+      </div>
     </div>
   );
 };
