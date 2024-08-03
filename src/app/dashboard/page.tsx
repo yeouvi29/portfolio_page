740b2c388d2a047549fd915c1c109b5ba3458b86
Emily Kang <yeouvi29@gmail.com>
2024-08-02 22:46:20 -0700

    style: add object-fit property to adjust image size

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,10 @@ const Page = () => {
         </p>
 
         <div className="rounded-2xl overflow-hidden">
-          <img src="/assets/san_francisco_image.webp" className="h-full" />
+          <img
+            src="/assets/san_francisco_image.webp"
+            className="h-full object-contain"
+          />
         </div>
         <div className="flex gap-2 flex-col min-[1248px]:flex-row items-center">
           <BartSection />
