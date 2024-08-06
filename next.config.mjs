423e7a728304da6a870a51cee1de87d89c9dfc1f
Emily Kang <yeouvi29@gmail.com>
2024-08-05 19:14:26 -0700

    chore: delete redirect setting

--- next.config.mjs
+++ next.config.mjs
@@ -3,23 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async redirects() {
-    if (process.env.NEXT_PUBLIC_DISABLE_REDIRECTS === "true") {
-      return [];
-    }
-    return [
-      {
-        source: "/api/:path*",
-        destination: "/api/:path*",
-        permanent: false,
-      },
-      {
-        source: "/:path((?!api/).*)",
-        destination: "https://portfolio.tokozzing.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
