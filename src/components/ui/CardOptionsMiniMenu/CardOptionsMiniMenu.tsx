4e6db8afcfa1bd9898ae241c85ed59377e1dd28a
Emily Kang <yeouvi29@gmail.com>
2024-08-09 20:10:44 -0700

    feat: add task delete function

--- src/components/ui/CardOptionsMiniMenu/CardOptionsMiniMenu.tsx
+++ src/components/ui/CardOptionsMiniMenu/CardOptionsMiniMenu.tsx
@@ -2,10 +2,13 @@
 
 import clsx from "clsx";
 import { Fragment, KeyboardEvent, useEffect, useRef, useState } from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
 import { AnimatePresence, motion } from "framer-motion";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { MdOutlineDelete } from "react-icons/md";
+import { IoWarning } from "react-icons/io5";
 
 import { useBreakpoint } from "@/hooks/useBreakpoint";
+import { useTaskItems } from "@/store";
 
 import MoveOptions from "../MoveOptions/MoveOptions";
 
@@ -27,6 +30,11 @@ const CardOptionsMiniMenu = ({
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [value, setValue] = useState<string>(task);
   const [showMoveOptions, setShowMoveOptions] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [taskItems, setTaskItems] = useTaskItems(({ items, setTaskItems }) => [
+    items,
+    setTaskItems,
+  ]);
   const { isDown } = useBreakpoint();
   const isMobileView = isDown("md");
   const handleChange = () => {
@@ -47,6 +55,23 @@ const CardOptionsMiniMenu = ({
       event.preventDefault();
       handleSubmit();
     }
+  };
+  const hideDeleteModal = () => {
+    setShowDeleteModal(false);
+  };
+  const deleteTask = () => {
+    const newTaskItems = taskItems.map((item) => {
+      if (item.id === columnId) {
+        return {
+          ...item,
+          items: item.items.filter((_, i) => i !== index),
+        };
+      }
+      return item;
+    });
+
+    setTaskItems(newTaskItems);
+    onClose();
   };
 
   useEffect(() => {
@@ -95,29 +120,79 @@ const CardOptionsMiniMenu = ({
           >
             Save
           </button>
-          <button
-            className="min-w-max flex gap-2 items-center bg-gray-300 rounded-md py-1 px-3 hover:bg-gray-400"
-            onClick={() => setShowMoveOptions((prev) => !prev)}
-          >
-            <span className="inline">
-              <FaArrowRightLong />
-            </span>
-            Move
-          </button>
-        </div>
-        <AnimatePresence>
-          {showMoveOptions && (
-            <motion.div
-              className="bg-gray-200 p-2 pt-3 mt-1 rounded-md w-full flex flex-col gap-y-2"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
+          <div>
+            <div className="relative">
+              <button
+                className="min-w-full flex gap-2 items-center bg-gray-300 rounded-md py-1 px-3 hover:bg-gray-400"
+                onClick={() => setShowMoveOptions((prev) => !prev)}
+              >
+                <span className="inline">
+                  <FaArrowRightLong />
+                </span>
+                Move
+              </button>
+              <AnimatePresence>
+                {showMoveOptions && (
+                  <motion.div
+                    className="absolute -bottom-2 right-0 translate-y-full w-[272px] bg-gray-200 p-2 rounded-md flex flex-col gap-y-2"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                  >
+                    <MoveOptions
+                      columnId={columnId}
+                      taskIndex={index}
+                      taskItems={taskItems}
+                      updateTaskItems={setTaskItems}
+                    />
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+            <button
+              className="min-w-full mt-2 flex gap-2 items-center bg-gray-300 rounded-md py-1 px-3 hover:bg-gray-400 text-red-500"
+              onClick={() => setShowDeleteModal((prev) => !prev)}
             >
-              <MoveOptions columnId={columnId} taskIndex={index} />
-            </motion.div>
-          )}
-        </AnimatePresence>
+              <span className="inline">
+                <MdOutlineDelete />
+              </span>
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
+      {showDeleteModal && (
+        <AnimatePresence>
+          <div
+            className="fixed inset-0 bg-black/50 flex justify-center items-center"
+            onClick={hideDeleteModal}
+          >
+            <div className="w-[300px] p-4 bg-white rounded-lg">
+              <p>
+                <span className="inline text-gray-600 text-2xl">
+                  <IoWarning />
+                </span>
+                Are you sure you want to delete this item? This action cannot be
+                undone.
+              </p>
+              <div className="flex w-full gap-2 mt-4">
+                <button
+                  className="flex-grow p-2 bg-gray-200 rounded-md hover:bg-gray-300"
+                  onClick={hideDeleteModal}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="flex-grow p-2 bg-gray-600 text-gray-300 rounded-md hover:bg-gray-700"
+                  onClick={deleteTask}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </AnimatePresence>
+      )}
     </Fragment>
   );
 };
